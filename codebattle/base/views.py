bc7ffe5995d49b9c046d64dce923d662cf987128
Andrew <ndrwrono2001@gmail.com>
2023-03-15 20:11:01 +0300

    enable adding and joining of participants

--- codebattle/base/views.py
+++ codebattle/base/views.py
@@ -78,6 +78,7 @@ def home_page(request):
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -87,10 +88,11 @@ def room(request,pk):
 
 
 
-        )   
+        )
+        room.participants.add(request.user)   
         return redirect('room', pk=room.id)
     
-    context={'room':room,'room_messages':room_messages}
+    context={'room':room,'room_messages':room_messages, 'participants':participants}
     return render(request,'room.html',context)
 
 @login_required(login_url='login')
