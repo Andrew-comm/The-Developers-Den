5466561cc3a7cfce687b194336503a7fc26db859
andrea <ndrwrono2001@gmail.com>
2024-04-03 23:20:20 +0300

    config for hosting

--- codebattle/codebattle/settings.py
+++ codebattle/codebattle/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,9 @@ STATICFILES_DIRS = [
     BASE_DIR /'static'
 ]
 LOGIN_URL = '/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
